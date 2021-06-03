172100275ff43d0378dec9319dd209da68204b59
ahsanf <ahsanfirdaus.2018@student.uny.ac.id>
2021-06-03 18:15:05 +0000

    update main_v1.py

--- Dockerfile
+++ Dockerfile
@@ -12,6 +12,8 @@ COPY . ./
 
 # Install production dependencies.
 RUN pip install Flask gunicorn
+RUN pip install pyzoom
+RUN pip install pyautogui
 
 # Run the web service on container startup. Here we use the gunicorn
 # webserver, with one worker process and 8 threads.
--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 export PROJECT_ID=zmood-314009
 gcloud builds submit --tag gcr.io/$PROJECT_ID/flaskapp_zm:v1
-gcloud run deploy flaskapp_zm --image gcr.io/$PROJECT_ID/flaskapp_zm:v1 --region asia-southeast2-a --platform managed
+gcloud run deploy flaskappzm --image gcr.io/$PROJECT_ID/flaskapp_zm:v1 --region asia-southeast2 --platform managed
