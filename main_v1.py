03ea9d21a33b4225a1be6f6aa6f4bf0738e88288
rifkyariy <rifkyariy@gmail.com>
2021-06-04 02:00:11 +0700

    change to POST

--- main_v1.py
+++ main_v1.py
@@ -1,19 +1,17 @@
 import json
 import shlex
-import os
 
 # zoom
 from pyzoom import ZoomClient
-#from bot import *
 
 from flask import Flask, request           # import flask
 app = Flask(__name__)             # create an app instance
 
 @app.route("/")
 def hello():
- return "Its Works" 
+ return "Its Works XX" 
 
-@app.route("/createMeeting") 
+@app.route("/createMeeting",methods=['POST']) 
 def createMeeting():
   data = request.get_json()
   
@@ -22,16 +20,16 @@ def createMeeting():
   client = ZoomClient(apiKey, apiSecret)
   
   topic = request.form['topic']
+#   start_time="2011-10-05T14:48:00.000Z"
   start_time = request.form['start_time']
+  duration_min = 60
   
-  response = str(
-    client.meetings.create_meeting(
-        topic, 
-        start_time, 
-        duration_min=60
+  response = str(client.meetings.create_meeting(
+    topic, 
+    start_time=start_time,
+    duration_min=duration_min
     )
   )
-  
   responses = shlex.split(response)
   
   tempAll = ""
@@ -45,15 +43,14 @@ def createMeeting():
   print('{'+str(tempAll[:-1])+'}')
   return(json.loads('{'+str(tempAll[:-1])+'}'))
 
-# @app.route("/triggerBot" ,methods=['GET']) 
-# def triggerBot():
-#   meetId = request.args.get('meetId')
-#   passCode = request.args.get('passCode')
-#   intervals = request.args.get('intervals')
+@app.route("/triggerBot" ,methods=['GET']) 
+def triggerBot():
+  meetId = request.args.get('meetId')
+  passCode = request.args.get('passCode')
+  intervals = request.args.get('intervals')
   
-#   startBot(meetId,passCode,intervals)
-#   return "success"
+  startBot(meetId,passCode,intervals)
+  return "success"
 
-#app.run()                    # run the flask app
-if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080))) 
+app.run()                    # run the flask app
+  
